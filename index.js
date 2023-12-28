651f3d896a1cc6677146987efdeae26594453865
Vansh Goyal <135618994+VanshGoyal000@users.noreply.github.com>
2023-12-28 16:25:08 +0530

    chai test

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+res.send("you learned hello world")
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>login page</h1>')
+    })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
